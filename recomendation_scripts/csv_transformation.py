2def41383d346200a6e2ef48fd83ab5042fc325b
JAKUB BROZ <Broz.Jk@gmail.com>
2022-06-14 14:39:36 +0200

    basic scripts

---
+++ recomendation_scripts/csv_transformation.py
@@ -0,0 +1,6 @@
+import csv
+
+
+def pick_attributes(file):
+    csv_file = file
+    return csv_file
---
+++ recomendation_scripts/recomendation_script.py
@@ -0,0 +1,2 @@
+from csv_transformation import pick_attributes
+csv = pick_attributes("data/movies_metadata.csv")
