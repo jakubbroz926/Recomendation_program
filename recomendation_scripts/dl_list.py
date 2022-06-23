6885fecbcee7babc1379afba58dff3a08911381d
JAKUB BROZ <Broz.Jk@gmail.com>
2022-06-23 08:33:14 +0200

    Structure file creation

---
+++ recomendation_scripts/dl_list.py
@@ -0,0 +1,11 @@
+class Node:
+
+    def __init__(self,category_name,data = None):
+        self.category_name = category_name
+        self.data = data
+
+
+class Double_List:
+
+    def __init__(self):
+        pass
--- recomendation_scripts/recomendation_script.py
+++ recomendation_scripts/recomendation_script.py
@@ -1,40 +1,51 @@
 import time
-
+from dl_list import Node,Double_List
+from heaps import Heap
 from csv_transformation import movies_attributes
 
 movie_of_genres = ['Animation', 'Comedy', 'Family', 'Adventure',
-                       'Fantasy', 'Romance', 'Drama', 'Action', 'Crime',
-                       'Thriller', 'Horror', 'History', 'Science Fiction',
-                       'Mystery', 'War', 'Music', 'Documentary',
-                       'Western']
-categories_of_films_numbers = {movie_of_genres.index(v) + 1:v for v in movie_of_genres}
+                   'Fantasy', 'Romance', 'Drama', 'Action', 'Crime',
+                   'Thriller', 'Horror', 'History', 'Science Fiction',
+                   'Mystery', 'War', 'Music', 'Documentary',
+                   'Western']
+categories_of_films_numbers = {movie_of_genres.index(v) + 1: v for v in movie_of_genres}
+
+
 def show_categories():
-        print("""There is plenty categories of movies.
-        Many movies could be find in more categories.""")
-        print(f"Possible categories and their type numbers are:\n")
-        for number, cat in categories_of_films_numbers.items():
-                time.sleep(0.5)
-                print(cat,number)
+    print("""There is plenty categories of movies.Many movies could be find in more categories.""")
+    print(f"Possible categories and their type numbers are:\n")
+    for number, cat in categories_of_films_numbers.items():
+        # time.sleep(0.5) Add after finishing program
+        print(cat, number)
+
+
 def select_category():
-        num = int(input("Select category by typing the its number."))
-        return num
+    num = int(input("Select category by typing the its number.\n:"))
+    return num
+
+
+def goodbye():
+    print("I hope you enjoyed our recommender and we hope u will enjoy your selected movie.\n"
+          "Bye.")
+
+
 def basic_menu():
     # vytvoření dvojitého listu z kategorií
-    dlinked_list_categories = [1,2,3,45,6]
+    dlinked_list_categories = [Node(movie_genre) for movie_genre in movie_of_genres]
     welcome_file = open("../data/welcome.txt", "r", encoding = "utf-8")
     print(welcome_file.read())
     welcome_file.close()
-    time.sleep(5)
+    # time.sleep(5) Add after finishing program
     show_categories()
     entry = select_category()
     while entry in categories_of_films_numbers.keys():
-        dlinked_list_categories(categories_of_films_numbers[entry])
-        #vypsání tří filmů
+        print(dlinked_list_categories[entry-1].category_name)
+        # attributes_of_movies = movies_attributes(r"../data/movies_metadata.csv")
+        # vypsání tří filmů
         answer = input("Are you satisfied? ")
-
-
-
-    attributes_of_movies = movies_attributes(r"../data/movies_metadata.csv")
+        if answer.lower() in ["y","yes"]:
+            break
+    goodbye()
 
 
 basic_menu()
