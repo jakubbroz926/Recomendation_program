a5c7b840e1e403dfbd0a6fef6707b2143a5ff61c
JAKUB BROZ <Broz.Jk@gmail.com>
2022-07-15 09:25:49 +0200

    Better results

--- recomendation_scripts/recomendation_script.py
+++ recomendation_scripts/recomendation_script.py
@@ -1,5 +1,5 @@
 import time
-from dl_list import Node,double_list,linked_list
+from dl_list import Node, double_list, linked_list
 from csv_transformation import movies_attributes
 
 movie_of_genres = ['Animation', 'Comedy', 'Family', 'Adventure',
@@ -35,30 +35,31 @@ def printing_categories(dl_list):
     return linked_list.printing_list(dl_list)
 
 
-def description(lst,number):
+def description(lst, number):
     for i in lst[number]:
         print(i)
     return
 
 
-def results(lst_of_movie_cat):
-    while lst_of_movie_cat:
-        print("The three best movies in this category are:")
-        for i,movie in enumerate(lst_of_movie_cat):
+def decision_tree(dl_list):
+    entry = select_category()
+    movies = dl_list.go_through(entry).get_data()
+    while movies:
+        for i,movie in enumerate(movies):
             print(movie[0],movie[1])
             if i == 2:
                 break
-        answer = input("If you want see description of these movies, type 1 or 2 or 3. If you want to see another movies type n, "
-                       "If you different movies of different category, type d."
-                       "For the end type x.\n:")
-        if answer.isdigit():
-            description(lst_of_movie_cat,int(answer)-1)
-        elif answer == "n":
-            results(lst_of_movie_cat[3:])
+        answer = input(
+            "For description of movie type 1 or 2 or 3.\nFor next movies in this category type n.\n"
+            "For different category type d.\n"
+            "For the quit type x.\n:")
+        if answer == "x":
+            return
         elif answer == "d":
-            return True
-        elif answer == "x":
-            return False
+            entry = select_category()
+            movies = dl_list.go_through(entry).get_data()
+        elif answer.isdigit():
+            print(movies[int(answer)][0]+"\n",movies[int(answer)][-1])
 
 
 def main():
@@ -68,21 +69,11 @@ def main():
     double_list_categories = double_list()
     for movie_of_genre in movie_of_genres:
         double_list_categories.add_tail(movie_of_genre)
-    movies_attributes(r"../data/movies_metadata.csv",double_list_categories)
+    movies_attributes(r"../data/movies_metadata.csv", double_list_categories)
     double_list_categories.sort_the_categories()
     time.sleep(2)
     show_categories()
-    entry = select_category()
-    while entry in categories_of_films_numbers.values():
-        our_movies = double_list_categories.go_through(entry).get_data()
-        if results(our_movies):
-            entry = select_category()
-            continue
-        answer = input("Are you satisfied? ")
-        if answer.lower() in ["y","yes"]:
-            break
-        else:
-            entry = select_category()
+    decision_tree(double_list_categories)
     goodbye()
 
 
